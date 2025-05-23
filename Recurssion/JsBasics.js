12ef21cf0c42e0e62f4e0f86cf92116af1e73d47
srihari <gatreddisrihari9550@gmail.com>
2025-05-23 10:46:41 +0530

    .

---
+++ Recurssion/JsBasics.js
@@ -0,0 +1,8 @@
+let a=5;
+const b=10;
+let name="Srihari";
+let age=30;
+let college="Raghu Enginerring College";
+
+console.log("Name ",name,"\nage ",age,"\nCollege ",college);
+console.log('Name')
