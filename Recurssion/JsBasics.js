0337fd79eacea5a6f6755f7f4106f7e602afa601
srihari <gatreddisrihari9550@gmail.com>
2025-05-24 09:21:25 +0530

    .

--- Recurssion/JsBasics.js
+++ Recurssion/JsBasics.js
@@ -1,8 +1,23 @@
 let a=5;
-const b=10;
+let b=10;
 let name="Srihari";
 let age=30;
 let college="Raghu Enginerring College";
 
 console.log("Name ",name,"\nage ",age,"\nCollege ",college);
-console.log('Name')
+console.log(`Name :${name}
+    Age:${age}
+    college:${college}`);
+
+console.log(a**b);
+console.log(a+b);
+console.log(a+=b);
+console.log(a);
+console.log(a,b);
+console.log(a>b);
+console.log(a>b && b<0);
+console.log(a<<b);
+console.log(a>>b)
+console.log("Sri"+" World");
+let msg= age>=30?"Adult":"Child";
+console.log(msg);
